de3c755ea06706b02f1f21fa25d55845b48d57a9
Mathieu Kapfer <mkapfer@prophesee.ai>
2025-06-30 17:10:30 +0200

    Minimum emacs conf

---
+++ init.el
@@ -0,0 +1,171 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dired-listing-switches "-alh")
+ '(dired-omit-files "^\\.?#\\|^\\.$\\|^\\.[^.]")
+ '(grep-files-aliases
+   '(("all" . "* .*")
+     ("el" . "*.el")
+     ("ch" . "*.[ch] *.cpp")
+     ("c" . "*.c")
+     ("cc" . "*.cc *.cxx *.cpp *.C *.CC *.c++")
+     ("cchh" . "*.cc *.[ch]xx *.[ch]pp *.[CHh] *.CC *.HH *.[ch]++")
+     ("hh" . "*.hxx *.hpp *.[Hh] *.HH *.h++")
+     ("h" . "*.h")
+     ("l" . "[Cc]hange[Ll]og*")
+     ("m" . "[Mm]akefile*")
+     ("tex" . "*.tex")
+     ("texi" . "*.texi")
+     ("asm" . "*.[sS]")))
+ '(lsp-clangd-binary-path "/usr/bin/clangd")
+ '(package-selected-packages
+   '(bash-completion vlf use-package abyss-theme lsp-mode seq magit))
+ '(warning-suppress-types '((comp) (comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; fix
+(setq package-install-upgrade-built-in t)
+
+;; edit init.el
+
+;; ==========================
+;; key short cut
+;; ==========================
+(defun switch-to-buffer-emacs ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el")
+  ;;(switch-to-buffer "init.el" )
+  )
+(global-set-key (kbd "C-<f1>") 'switch-to-buffer-emacs)
+
+;; window split helper in french keyboard
+(global-set-key (kbd "M-œ")  'delete-window)
+(global-set-key (kbd "M-&")  'delete-other-windows )
+(global-set-key (kbd "M-é")  'split-window-vertically)
+(global-set-key (kbd "M-\"") 'split-window-horizontally)
+
+;; windows navigation
+(global-set-key (kbd "C-<tab>") 'other-window)
+(global-set-key (kbd "C-œ") 'other-window)
+
+;; buffer navigation
+(global-set-key (kbd "M-<left>") 'previous-buffer) ; ALT+ flèche gauche
+(global-set-key (kbd "M-<right>")  'next-buffer) ; ALT + flèche droite
+
+;; shortcut for developement
+(global-set-key [f1] 'dired-sources-file)
+(global-set-key [f12] 'compile)
+(global-set-key [(control f12)] 'recompile)
+(global-set-key [f11] 'shell)
+(global-set-key (kbd "<f9>") 'my-grep)
+
+;; windows short cut
+(global-set-key (kbd "C-z") 'undo)
+
+;; magit short cut
+(global-set-key (kbd "C-<escape>") 'magit-status)
+
+
+(defun dired-sources-file ()
+  (interactive)
+  (dired default-directory)
+  ;;(replace-in-string (buffer-file-name) "/[^/]+$" "/."))
+)
+
+;; ==========================
+;; nice
+;; ==========================
+
+;; theme
+(abyss-theme)
+
+
+;; dired
+(require 'dired-x)
+(add-hook 'dired-mode-hook (lambda () (dired-omit-mode)))
+;; and simplier shortcut
+(global-set-key (kbd "M-o") 'dired-omit-mode)
+
+;; ansicolor in compile buffer
+;(require 'ansi-color)
+;(defun colorize-compilation-buffer ()
+;  (toggle-read-only)
+;  (ansi-color-apply-on-region compilation-filter-start (point))
+;  (toggle-read-only))
+;(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+;; get line number everywhere
+(global-display-line-numbers-mode)
+
+
+;; ==========================
+;; lsp mode
+;; ==========================
+(use-package lsp-mode
+    :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+            (c-mode . lsp-deferred)
+            (c++-mode . lsp-deferred)
+            (python-mode . lsp-deferred)
+            (java-mode . lsp-deferred)
+            ;; if you want which-key integration
+            (lsp-mode . lsp-enable-which-key-integration))
+    :commands lsp-deferred
+    :custom (lsp-clients-clangd-executable
+             ;; from https://github.com/clangd/clangd/releases/tag/11.0.0
+             ;; "~/.emacs.d/lsp-server/clangd_11.0.0/bin/clangd")
+             ;; "/home/mkapfer/.emacs.d/lsp-server/clangd_11.0.0/bin/clangd"
+             "/usr/bin/clangd"
+             )
+    )
+
+
+;; ==========================
+;; package
+;; ==========================
+
+;; packages
+(setq package-archives '(
+;;;                         ("melpa-stable" . "http://stable.melpa.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         )
+      )
+
+(put 'erase-buffer 'disabled nil)
+
+
+
+;; ==========================
+;; C++ ide
+;; ==========================
+
+;; force c++ mode for *.h files
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+
+;; ==========================
+;; C++ ide
+;; ==========================
+(add-to-list 'load-path "/home/psee/.emacs.d/lisp")
+(require 'evt-decoder)
+
+
+;; ==========================
+;; improve shell
+;; ==========================
+;; adding search in history
+(require 'shell)
+(define-key shell-mode-map (kbd "C-r") 'comint-history-isearch-backward)
+
+(use-package bash-completion
+  :ensure t
+  :config
+  (bash-completion-setup))
---
+++ lisp/evt-decoder.el
@@ -0,0 +1,175 @@
+;; unit test framwork
+;; (require 'ert)
+
+
+;;; Code:
+(defconst event-type-table-evt21
+      '(("0" . "D-")
+        ("1" . "D+")
+        ("8" . "TH")
+        ("A" . "T!")
+        ("E" . "*" )
+        )
+      "Table of event type in EVT2.1 format."
+      )
+
+(defconst event-type-table-evt30
+      '(("0" . "CDY")
+        ("2" . "XPOS")
+        ("3" . "XBAS")
+        ("4" . "V12")
+        ("5" . "V8")
+        ("6" . "TL")
+        ("8" . "TH")
+        ("A" . "T!")
+        ("E" . "*" )
+        )
+      "Table of event type in EVT3.0 format."
+      )
+
+
+(defvar hex-dump-highlight-colors
+  '(
+    ;; Common
+    ("TL" . "#000070")
+    ("TH" . "#700070")
+    ("T!" . "#F00000")
+    ("*"  . "#707070")
+    ;; EVT21 only
+    ("D-" . "#007000")
+    ("D+" . "#700000")
+    ;; EVT30 only
+    ("V8"  . "green")
+    ("V12" . "dark-green")
+    )
+  "Backgroud color for each type.")
+
+
+;; helper to get one hexadecimal byte string with optional space
+(defconst hex-digit-byte (concat
+                     "\\(?:"                                   ;; start shy group
+                     "[[:xdigit:]][[:xdigit:]][[:space:]]?"    ;; "XX" with option space at the end
+                     "\\)"                                     ;; stop shy group
+                     ))
+
+;; helper to swap endiannes
+(defun swap-endianness (hex-string)
+  "Convert endianness of HEX-STRING in 4-byte (8 hex digits) chunks.
+Input may optionally contain spaces between bytes."
+  (let* ((cleaned (replace-regexp-in-string "[ \t\n]+" "" hex-string))  ; remove all whitespace
+         (result "")
+         (i 0))
+    (let* ((bytes (seq-reverse (seq-partition cleaned 2)))
+           (reversed (apply #'concat bytes)))
+      (setq result (concat result reversed)))
+    result))
+
+;; unit test
+(ert-deftest test-swap-endianness ()
+  (should (string-equal (swap-endianness "00 00 00 00 E1 35 04 80") "800435E100000000" ))
+  (should (string-equal (swap-endianness "00 40 00 00 00 00 C4 12") "12C4000000004000" ))
+  ;; with optional space
+  (should (string-equal (swap-endianness "00400000 0000C412") "12C4000000004000" ))
+  (should (string-equal (swap-endianness "0000 0000 E135 0480") "800435E100000000" ))
+  )
+
+
+;; evt21 decoder
+(defun decode-evt21 (hex-string-little-endian)
+  "Decode event type and related value:
+    - input: hexa string in big endian (starting with MSB) without space
+    - output: list with event type name and value
+   Example: \"800435E100000000\" => (\"TH\". #x435E1)
+"
+  (let* (
+         (hex-string-big-endian (swap-endianness hex-string-little-endian))
+         (type-nibble (substring hex-string-big-endian 0 1))
+         (type (cdr (assoc type-nibble event-type-table-evt21)))
+         (value-str (substring hex-string-big-endian 1 8))
+         (value (string-to-number value-str 16)))
+    (cons (or type "?") value))
+  )
+
+;; unit test
+(ert-deftest test-decode-evt21 ()
+  (should (equal (decode-evt21 "00 00 00 00 01 00 00 80") '("TH" . 1)))
+  (should (equal (decode-evt21 "00 00 00 00 10 00 00 80") '("TH" . 16)))
+  (should (equal (decode-evt21 "00 00 00 00 00 01 00 80") '("TH" . 256)))
+  (should (equal (decode-evt21 "00 00 00 00 E1 35 04 80") '("TH" . #x435E1)))
+  (should (equal (decode-evt21 "00 00 00 00 00 01 00 00") '("D-" . 256)))
+  )
+
+;; evt30 decoder
+(defun decode-evt30 (hex-string-little-endian)
+  "Decode event type and related value:
+    - input: hexa string in big endian (starting with MSB) without space
+    - output: list with event type name and value
+   Example: \"800435E100000000\" => (\"TH\". #x435E1)
+"
+  (let* (
+         (hex-string-big-endian (swap-endianness hex-string-little-endian))
+         (type-nibble (substring hex-string-big-endian 0 1))
+         (type (cdr (assoc type-nibble event-type-table-evt30)))
+         (value-str (substring hex-string-big-endian 1 4))
+         (value (string-to-number value-str 16)))
+    (cons (or type "?") value))
+  )
+
+;; unit test
+(ert-deftest test-decode-evt30 ()
+  (should (equal (decode-evt30 "c4 81") '("TH" . #x1c4)))
+  (should (equal (decode-evt30 "c481") '("TH" . #x1c4)))
+  )
+
+;; wrapper
+(defun my-hex-decode-fn (hex-string)
+  (decode-evt30 hex-string)
+  )
+
+;; main function that parse the buffer to highlight
+(defun evt-highlight-hex-blocks ()
+  "Parcourt le buffer et applique une coloration de fond aux blocs de 8 octets."
+  (interactive)
+  (save-excursion
+    ;;(goto-char (point-min))
+    (let ((case-fold-search nil))
+      ;; Recherche de lignes avec dump hexadécimal.
+      (while (re-search-forward
+              (concat
+               ;; 00000: 00 00 00 00 45 F2 68 00 00 00 00 00 46 F2 68 80
+               "^[[:xdigit:]]+:[[:space:]]+"             ;; "0000: "
+               "\\("                                     ;; start numbered group 1
+               hex-digit-byte                            ;; "XX "
+               "\\{16\\}"                                ;; 16 times
+               "\\)"                                     ;; stop numbered group 1
+               )
+              nil t)
+        (let ((start (match-beginning 1))
+              (end (match-end 1)))
+          (save-excursion
+            (goto-char start)
+            ;; Découpage en blocs de 4 octets
+            (dotimes (i 8)
+              (let* ((event-str-search
+                      (re-search-forward (concat "\\(" hex-digit-byte "\\{2\\}" "\\)")))
+                     (block-start (match-beginning 1))
+                     (block-end (match-end 1))
+                     (hex-block (buffer-substring-no-properties block-start block-end))
+                     (type-value (my-hex-decode-fn hex-block))
+                     (type (car type-value))
+                     (face-color (or (cdr (assoc type hex-dump-highlight-colors)) "darkgray"))
+                     (decoded-content (format "%-4s" type))
+                     ;;; (decoded-content (format "%-3s: %08x" type (cdr type-value)))
+                     )
+                (when (<= block-end (point-max))
+                  (save-excursion
+                    (end-of-line)
+                    (insert (concat "  " decoded-content)
+                    )
+                  (let ((ov (make-overlay block-start block-end)))
+                    (overlay-put ov 'face `(:background ,face-color))
+                    (overlay-put ov 'help-echo decoded-content))))))))))
+    )
+  )
+
+(provide 'evt-decoder)
